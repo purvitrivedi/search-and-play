0d9441b8f53dbe309802257aee4e16d61fbe0b4a
purvitrivedi <purvitriv@gmail.com>
2020-05-06 11:35:31 +0100

    home

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -13,13 +13,10 @@ class Home extends React.Component {
     },
     validate: true,
     images: [],
-    imageSrc: 'https://is2-ssl.mzstatic.com/image/thumb/Music/v4/f0/21/75/f021755c-d4ac-42fe-6e1d-33e08735c8c1/source/100x100bb.jpg',
-    imageSrc2: 'https://is1-ssl.mzstatic.com/image/thumb/Music69/v4/4e/b8/ba/4eb8ba96-6ee2-8a60-f012-a7a61a4fe097/source/100x100bb.jpg',
-    imageSrc3: 'https://is2-ssl.mzstatic.com/image/thumb/Music7/v4/14/1c/69/141c691f-e0bf-0a5f-6f49-092d59837279/source/100x100bb.jpg',
-    imageSrc4: 'https://is2-ssl.mzstatic.com/image/thumb/Music118/v4/f4/58/47/f4584702-bd77-52c3-f7e0-fa05f4e80397/source/100x100bb.jpg'
-
-
-
+    imageSrc: '',
+    imageSrc2: '',
+    imageSrc3: '',
+    imageSrc4: ''
 
   }
 
